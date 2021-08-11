e5363ba353146cda3a1bfbf01c517915b278b425
Soham Kulkarni <sohamkrox@gmail.com>
2021-08-12 01:58:43 +0530

    Reformat Code

--- test.py
+++ test.py
@@ -1,26 +1,22 @@
+from collections import Counter
 import liwc
 import re
 parse, category_names = liwc.load_token_parser('LIWC2007_English100131.dic')
 
-print('no. categories:',len(category_names))
+print('no. categories:', len(category_names))
+
 
 def tokenize(text):
     # you may want to use a smarter tokenizer
     for match in re.finditer(r'\w+', text, re.UNICODE):
         yield match.group(0)
 
-gettysburg = '''Four score and seven years ago our fathers brought forth on
-  this continent a new nation, conceived in liberty, and dedicated to the
-  proposition that all men are created equal. Now we are engaged in a great
-  civil war, testing whether that nation, or any nation so conceived and so
-  dedicated, can long endure. We are met on a great battlefield of that war.
-  We have come to dedicate a portion of that field, as a final resting place
-  for those who here gave their lives that that nation might live. It is
-  altogether fitting and proper that we should do this.'''.lower()
+
+gettysburg = '''It was a big company so luckily I didn't have to see him all the time, but when I did, he again acted as though I didn't exist. I tried to talk to him and update him on the pregnancy, and ask him to be involved for our child's sake, and he literally stared right through me without saying a word. 3 months after our daughter was born, I texted him and told him he was missing out on the most amazing person in this world, and how it wasn't fair to her. To my surprise, he wrote back and wanted to see her. He apologized, said he had changed and within a couple of months, we were engaged.'''.lower()
 
 gettysburg_tokens = tokenize(gettysburg)
 
-from collections import Counter
-gettysburg_counts = Counter(category for token in gettysburg_tokens for category in parse(token))
-print(gettysburg_counts)    
-print(len(gettysburg_counts))    
+gettysburg_counts = Counter(
+    category for token in gettysburg_tokens for category in parse(token))
+print(gettysburg_counts)
+print(len(gettysburg_counts))
